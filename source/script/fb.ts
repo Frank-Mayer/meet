79423484da59cb8631572bfd05b4f1a72ba12120
Frank-Mayer <git@frank-mayer.io>
2023-05-25 16:03:23 +0200

    update firebase

--- source/script/fb.ts
+++ source/script/fb.ts
@@ -8,14 +8,13 @@ import {
 } from "firebase/firestore";
 import { panic } from "./error";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC9sshzAX6ZUwLVVoORz7A6LfS_8nEBFhw",
-    authDomain: "meet--now.firebaseapp.com",
-    projectId: "meet--now",
-    storageBucket: "meet--now.appspot.com",
-    messagingSenderId: "302409493880",
-    appId: "1:302409493880:web:cbe109600231fc1034ec8d",
+    apiKey: "AIzaSyCa9xv3IO7BscgKIFdHsz8RDRpdvQ3tEAk",
+    authDomain: "meet-56fd5.firebaseapp.com",
+    projectId: "meet-56fd5",
+    storageBucket: "meet-56fd5.appspot.com",
+    messagingSenderId: "717136743724",
+    appId: "1:717136743724:web:982f89212a552fb1ad3089",
 };
 
 // Initialize Firebase
@@ -34,35 +33,39 @@ export type Vote = {
 };
 
 /** ensure that room exists and return it */
-export async function getRoom (collectionName: string, meta?: RoomMeta) {
+export async function getRoom(collectionName: string, meta?: RoomMeta) {
     const room = collection(fs, collectionName);
     if (meta) {
         const roomMeta = doc(room, "meta");
         await setDoc(roomMeta, meta);
     }
     return room;
-};
+}
 
 /** get users in the room */
-export async function getUsers (roomLocation: string) {
+export async function getUsers(roomLocation: string) {
     const room = await getRoom(roomLocation);
     const users = doc(room, "users");
     const usersData = await getDoc(users);
     return usersData.data();
-};
+}
 
 /** add user to the room */
-export async function addUser (roomLocation: string, user: string) {
+export async function addUser(roomLocation: string, user: string) {
     const room = await getRoom(roomLocation);
     const users = doc(room, "users");
     const usersData = await getDoc(users);
     const usersDataData = usersData.data();
     const newUsers = { ...usersDataData, [user]: [] };
     await setDoc(users, newUsers);
-};
+}
 
 /** set votes of user in room */
-export async function setVotes (roomLocation: string, user: string, votes: Array<Vote>) {
+export async function setVotes(
+    roomLocation: string,
+    user: string,
+    votes: Array<Vote>,
+) {
     const room = await getRoom(roomLocation);
     const users = doc(room, "users");
     const usersData = await getDoc(users);
@@ -72,7 +75,10 @@ export async function setVotes (roomLocation: string, user: string, votes: Array
 }
 
 /** get votes of user in room */
-export async function getVotes (roomLocation: string, user: string): Promise<Array<Vote>> {
+export async function getVotes(
+    roomLocation: string,
+    user: string,
+): Promise<Array<Vote>> {
     const room = await getRoom(roomLocation);
     const users = doc(room, "users");
     const usersData = await getDoc(users);
@@ -81,7 +87,9 @@ export async function getVotes (roomLocation: string, user: string): Promise<Arr
 }
 
 /** get all votes in a room grouped by user */
-export async function getAllVotes (roomLocation: string): Promise<Record<string, Array<Vote>>> {
+export async function getAllVotes(
+    roomLocation: string,
+): Promise<Record<string, Array<Vote>>> {
     const room = await getRoom(roomLocation);
     const users = doc(room, "users");
     const usersData = await getDoc(users);
@@ -90,7 +98,7 @@ export async function getAllVotes (roomLocation: string): Promise<Record<string,
 }
 
 /** get meta of room */
-export async function getMeta (roomLocation: string): Promise<RoomMeta> {
+export async function getMeta(roomLocation: string): Promise<RoomMeta> {
     const room = await getRoom(roomLocation);
     const meta = doc(room, "meta");
     const metaDoc = await getDoc(meta);
